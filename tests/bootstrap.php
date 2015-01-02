7ceec11e2e5b5ac1d0622f842b4b5c9cfb707a7a
Lars Moelleken <lars@moelleken.org>
2015-01-02 12:28:35 +0100

    [~]: try to fix "travis-ci" build v4.2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,3 @@
 <?php
-if (is_file(dirname(__DIR__) . '/src/voku/helper/UTF8.php')) {
-  # for netbeans
-  require_once dirname(__DIR__) . '/vendor/voku/urlify/URLify.php';
-  require_once dirname(__DIR__) . '/src/voku/helper/UTF8.php';
-} else {
-  # for travis-ci
-  require_once dirname(__DIR__) . '/vendor/composer/autoload.php';
-}
+
+require_once dirname(__DIR__) . '/vendor/composer/autoload_classmap.php';
