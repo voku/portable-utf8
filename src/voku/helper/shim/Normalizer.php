3a64c7bf8a8d20befc7d6210a5ed3d915d3d54d7
Lars Moelleken <lmoelleken@menadwork.com>
2015-03-01 03:42:48 +0100

    [+] edit "Normalizer.php"-Class -> added php-doc

--- src/voku/helper/shim/Normalizer.php
+++ src/voku/helper/shim/Normalizer.php
@@ -41,8 +41,10 @@ class Normalizer
   protected static $ASCII = "\x20\x65\x69\x61\x73\x6E\x74\x72\x6F\x6C\x75\x64\x5D\x5B\x63\x6D\x70\x27\x0A\x67\x7C\x68\x76\x2E\x66\x62\x2C\x3A\x3D\x2D\x71\x31\x30\x43\x32\x2A\x79\x78\x29\x28\x4C\x39\x41\x53\x2F\x50\x22\x45\x6A\x4D\x49\x6B\x33\x3E\x35\x54\x3C\x44\x34\x7D\x42\x7B\x38\x46\x77\x52\x36\x37\x55\x47\x4E\x3B\x4A\x7A\x56\x23\x48\x4F\x57\x5F\x26\x21\x4B\x3F\x58\x51\x25\x59\x5C\x09\x5A\x2B\x7E\x5E\x24\x40\x60\x7F\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
 
   /**
-   * @param     $s
-   * @param int $form
+   * is normalized
+   *
+   * @param string $s
+   * @param int    $form
    *
    * @return bool
    */
@@ -60,10 +62,12 @@ class Normalizer
   }
 
   /**
-   * @param     $s
-   * @param int $form
+   * normalize
    *
-   * @return bool|string
+   * @param string $s
+   * @param int    $form
+   *
+   * @return bool|string false on error
    */
   public static function normalize($s, $form = self::NFC)
   {
@@ -118,7 +122,9 @@ class Normalizer
   }
 
   /**
-   * @param $file
+   * getData
+   *
+   * @param string $file
    *
    * @return bool|mixed
    */
@@ -133,7 +139,9 @@ class Normalizer
   }
 
   /**
-   * @param $s
+   * recompose
+   *
+   * @param string $s
    *
    * @return string
    */
@@ -228,8 +236,10 @@ class Normalizer
   }
 
   /**
-   * @param $s
-   * @param $c
+   * decompose
+   *
+   * @param string $s
+   * @param bool   $c
    *
    * @return string
    */
