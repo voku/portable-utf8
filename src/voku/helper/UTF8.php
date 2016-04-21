9f4e7de5970be95a27e00ac611741652e5daf62f
Lars Moelleken <lars@moelleken.org>
2016-04-21 09:45:33 +0200

    Revert "修改正则匹配规则"
    This reverts commit f6686860d67c0a0258302aa25ddc5a3fb5758e6d.

--- src/voku/helper/UTF8.php
+++ src/voku/helper/UTF8.php
@@ -1588,19 +1588,18 @@ class UTF8
     // http://stackoverflow.com/questions/1401317/remove-non-utf8-characters-from-string
     // caused connection reset problem on larger strings
 
-    $regx = <<<END
-/
-  (
-    (?: [\x00-\x7F]               # single-byte sequences   0xxxxxxx
-    |   [\xC0-\xDF][\x80-\xBF]    # double-byte sequences   110xxxxx 10xxxxxx
-    |   [\xE0-\xEF][\x80-\xBF]{2} # triple-byte sequences   1110xxxx 10xxxxxx * 2
-    |   [\xF0-\xF7][\x80-\xBF]{3} # quadruple-byte sequence 11110xxx 10xxxxxx * 3
-    ){1,100}                      # ...one or more times
-  )
-| ( [\x80-\xBF] )                 # invalid byte in range 10000000 - 10111111
-| ( [\xC0-\xFF] )                 # invalid byte in range 11000000 - 11111111
-/x
-END;
+    $regx = '/
+       (
+        (?: [\x00-\x7F]                  # single-byte sequences   0xxxxxxx
+        |   [\xC2-\xDF][\x80-\xBF]       # double-byte sequences   110xxxxx 10xxxxxx
+        |   \xE0[\xA0-\xBF][\x80-\xBF]   # triple-byte sequences   1110xxxx 10xxxxxx * 2
+        |   [\xE1-\xEC][\x80-\xBF]{2}
+        |   \xED[\x80-\x9F][\x80-\xBF]
+        |   [\xEE-\xEF][\x80-\xBF]{2}
+        ){1,50}                          # ...one or more times
+       )
+       | .                               # anything else
+       /x';
     $str = preg_replace($regx, '$1', $str);
 
     $str = self::replace_diamond_question_mark($str, '');
