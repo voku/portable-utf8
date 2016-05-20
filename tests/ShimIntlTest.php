748992dd54e3b9af2349d1ed277b99fb3acb0c93
Lars Moelleken <lars@moelleken.org>
2016-05-20 11:12:22 +0200

    [+]: "Fix tests for php 7.0.6"
    -> https://github.com/symfony/polyfill/commit/99a2aebae1eb7e94df94d01801deb2897dfc127a

--- tests/ShimIntlTest.php
+++ tests/ShimIntlTest.php
@@ -104,7 +104,11 @@ class ShimIntlTest extends PHPUnit_Framework_TestCase
     self::assertSame(false, grapheme_strpos('abc', ''));
     self::assertSame(false, grapheme_strpos('abc', 'd'));
     self::assertSame(false, grapheme_strpos('abc', 'a', 3));
-    self::assertSame(0, grapheme_strpos('abc', 'a', -1));
+    if (defined('HHVM_VERSION_ID') || PHP_VERSION_ID < 50535 || (50600 <= PHP_VERSION_ID && PHP_VERSION_ID < 50621) || (70000 <= PHP_VERSION_ID && PHP_VERSION_ID < 70006)) {
+      $this->assertSame(0, grapheme_strpos('abc', 'a', -1));
+    } else {
+      $this->assertFalse(grapheme_strpos('abc', 'a', -1));
+    }
     self::assertSame(1, grapheme_strpos('한국어', '국'));
     self::assertSame(3, grapheme_stripos('DÉJÀ', 'à'));
     self::assertSame(false, grapheme_strrpos('한국어', ''));
@@ -114,7 +118,11 @@ class ShimIntlTest extends PHPUnit_Framework_TestCase
     self::assertSame(false, p::grapheme_strpos('abc', ''));
     self::assertSame(false, p::grapheme_strpos('abc', 'd'));
     self::assertSame(false, p::grapheme_strpos('abc', 'a', 3));
-    self::assertSame(0, p::grapheme_strpos('abc', 'a', -1));
+    if (defined('HHVM_VERSION_ID') || PHP_VERSION_ID < 50535 || (50600 <= PHP_VERSION_ID && PHP_VERSION_ID < 50621) || (70000 <= PHP_VERSION_ID && PHP_VERSION_ID < 70006)) {
+      $this->assertSame(0, p::grapheme_strpos('abc', 'a', -1));
+    } else {
+      $this->assertFalse(p::grapheme_strpos('abc', 'a', -1));
+    }
     self::assertSame(1, p::grapheme_strpos('한국어', '국'));
     self::assertSame(3, p::grapheme_stripos('DÉJÀ', 'à'));
     self::assertSame(false, p::grapheme_strrpos('한국어', ''));
