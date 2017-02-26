deb5db5a79f27d473c0e76648b0f4947f0c4f758
Lars Moelleken <lars@moelleken.org>
2017-02-26 18:45:18 +0100

    [!]: use "strict_types=1" for PHP >= 7.0 v2.1

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,6 @@
 <?php
 
-if (version_compare(PHP_VERSION, '7.0', '>=')) { // "strict_types=1" === warning with PHP < 7.0
-  error_reporting(E_ALL);
-}
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
